89125f36d66cb59f2785227e8857112404d1e390
Mehedi Hasan <mdmehedihasan20010@gmail.com>
2025-01-11 23:10:02 +0600

    redux quiz pro

---
+++ src/home/QuestionsController.tsx
@@ -0,0 +1,3 @@
+export default function QuestionsController() {
+  return <div>QuestionsController</div>;
+}
---
+++ src/redux/features/quiz/quizSlice.ts
@@ -0,0 +1,7 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const quizSlice = createSlice({
+  name: "quizs",
+  initialState,
+  reducers,
+});
